cab70fa06d107e0c70761e387c5427a9707dd357
Marko Fluit <marko.fluit@ing.nl>
2018-02-23 22:37:30 +0100

    initial commit

---
+++ src/main/java/nl/fluitit/api/termpayments/TermPaymentsApiApplication.java
@@ -0,0 +1,12 @@
+package nl.fluitit.api.termpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermPaymentsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermPaymentsApiApplication.class, args);
+	}
+}
