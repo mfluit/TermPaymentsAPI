0e6b52d545ba450453c21badaeadb87646b557df
Marko Fluit <marko.fluit@ing.nl>
2018-02-23 23:05:58 +0100

    instead of specifying every endpoint we now scan the resources package for endpoints that need to be registered

--- src/main/java/nl/fluitit/api/termpayments/JerseyConfig.java
+++ src/main/java/nl/fluitit/api/termpayments/JerseyConfig.java
@@ -1,15 +1,15 @@
 package nl.fluitit.api.termpayments;
 
-import nl.fluitit.api.termpayments.resources.TermPaymentsResource;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import javax.ws.rs.ApplicationPath;
 
-@Component
+@Configuration
 @ApplicationPath("api")
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
-        register(TermPaymentsResource.class);
+        // scans the resources package for our resources
+        packages(getClass().getPackage().getName() + ".resources");
     }
 }
