f353c3ff83a178de0decee55988de442cd9751a9
Marko Fluit <marko.fluit@ing.nl>
2018-02-23 22:43:38 +0100

    renamed main application class

--- src/main/java/nl/fluitit/api/termpayments/Application.java
+++ src/main/java/nl/fluitit/api/termpayments/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TermPaymentsApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TermPaymentsApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
